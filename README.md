49f254ddb7bf54a6b55cd6d906c2f3079b7cfe27
r-yui <gcupcake79@gmail.com>
2024-02-02 22:15:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# rodas-appdev-mvc-prelim" 
---
+++ myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, HttpResponse
+from.models import TodoItem
+
+# Create your views here.
+
+def home(request):
+  return render(request, "home.html")
+
+def todos(request):
+  items = TodoItem.objects.all()
+  return render(request, "todos.html", {"todos": items})
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
